7b5a448e1b50e7eaa68d715fce536e12281ce5a2
Matthew Maynes <matthew.maynes@yahoo.ca>
2017-02-27 19:35:46 -0500

    Closes #20 - Added Spring Framework Launcher
    - Added Spring dependencies to pom.xml
    - Created auto-configured launcher class

--- src/test/java/org/sysc/ama/LauncherTest.java
+++ src/test/java/org/sysc/ama/LauncherTest.java
@@ -4,17 +4,14 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest extends TestCase {
+public class LauncherTest extends TestCase {
 
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName ) {
+    public LauncherTest (String testName) {
         super( testName );
     }
 
@@ -22,13 +19,10 @@ public class AppTest extends TestCase {
      * @return the suite of tests being tested
      */
     public static Test suite() {
-        return new TestSuite( AppTest.class );
+        return new TestSuite(LauncherTest.class);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue( true );
+    public void testAlwaysPass() {
+        assertTrue(true);
     }
 }
