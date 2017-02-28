286d1ca808a9b446182f561b86cb1bebef053463
ctblanch2 <ctblanch@gmail.com>
2017-02-27 20:31:51 -0500

    Make AMA unit tests do something useful

--- src/test/java/org/sysc/ama/model/TestAMA.java
+++ src/test/java/org/sysc/ama/model/TestAMA.java
@@ -10,24 +10,32 @@ import org.sysc.ama.model.User;
  * Created by Cameron on 27/02/2017.
  */
 public class TestAMA {
+
+    @Test
+    public void testGetAmaTitle() throws Exception {
+        User user = new User();
+        Ama ama = new Ama("Test AMA", user, true);
+        assertEquals("Test AMA", ama.getTitle());
+    }
+
     @Test
     public void amaShouldBeLinkedToUser() throws Exception {
         User user = new User();
         Ama ama = new Ama("Test AMA", user, true);
-        assertEquals(ama.getSubject(), user);
+        assertEquals(user, ama.getSubject());
     }
 
     @Test
     public void publicAmaShouldBePublic() throws Exception {
         User user = new User();
         Ama ama = new Ama("Test AMA", user, true);
-        assertEquals(ama.getSubject(), user);
+        assertEquals(true, ama.isPublic());
     }
 
     @Test
     public void privateAmaShouldBePrivate() throws Exception {
         User user = new User();
         Ama ama = new Ama("Test AMA", user, false);
-        assertEquals(ama.getSubject(), user);
+        assertEquals(false, ama.isPublic());
     }
 }
