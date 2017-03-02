29e2fedfd14ecd2b6386011bbbffc9c396d33376
Peter Mark <peter_mark12@hotmail.com>
2017-03-02 10:12:51 -0500

    fixed formatting

--- src/main/resources/public/ama.js
+++ src/main/resources/public/ama.js
@@ -1,1 +1,12 @@
-$("<h1>Hello World</h1>");
+webix.ui({
+  rows:[
+      { view:"template", 
+        type:"header", template:"My App!" },
+      { view:"datatable", 
+        autoConfig:true, 
+        data:{
+          title:"My Fair Lady", year:1964, votes:533848, rating:8.9, rank:5
+        }
+      }
+  ]
+});
