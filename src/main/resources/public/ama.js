405dcb08f4e29658b28ef8c4c38766fc824b388f
Peter Mark <peter_mark12@hotmail.com>
2017-03-02 12:25:32 -0500

    initial untested guarenteed broken stuff

--- src/main/resources/public/ama.js
+++ src/main/resources/public/ama.js
@@ -1,14 +1,17 @@
 document.onreadystatechange = function(){
-	webix.ui({
-	  rows:[
-	      { view:"template", 
-	        type:"header", template:"My App!" },
-	      { view:"datatable", 
-	        autoConfig:true, 
-	        data:{
-	          title:"My Fair Lady", year:1964, votes:533848, rating:8.9, rank:5
-	        }
-	      }
-	  ]
-	});
+	
+	webix.ajax.get("/ama/list", function(amas)) {
+		webix.ui({
+			rows: [
+			  { view: "template",
+			  type:"header", template:"List of AMAs" },
+			  { view:"datatable" ,
+				autoConfig:true,
+				data: {
+					amas
+			    }
+			  }
+		  ]
+		});
+	}
 }
