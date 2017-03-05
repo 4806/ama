575f956cc74ea8ce33d7ceb4d94738da1aa48aaa
ctblanch2 <ctblanch@gmail.com>
2017-03-05 18:36:31 -0500

    Log name of currently logged in user

--- src/main/java/org/sysc/ama/controller/UserController.java
+++ src/main/java/org/sysc/ama/controller/UserController.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
