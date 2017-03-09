4d634a0b25268c70444cebf9d00b8ab8e40960f3
Matthew Maynes <matthew.maynes@yahoo.ca>
2017-03-09 10:52:34 -0500

    Added test cases for models

--- src/test/java/org/sysc/ama/model/AnswerTest.java
+++ src/test/java/org/sysc/ama/model/AnswerTest.java
@@ -6,38 +6,42 @@ import org.junit.Test;
 
 import java.util.Date;
 
-
-public class QuestionTest {
+public class AnswerTest {
 
     private User author;
     private User amaSubject;
     private Ama  ama;
+    private Question question;
 
     @Before
     public void setUp () {
         this.author     = new User();
         this.amaSubject = new User();
         this.ama        = new Ama("Foo", this.amaSubject, true);
+        this.question   = new Question(this.author, this.ama, "How?");
     }
 
     @Test
-    public void testCreateQuestion () {
+    public void testCreateAnswer () {
         String body = "How is the weather?";
 
         // For notes on timing, see comment in PostTest.testCreatePost
         Date beforeCreated = new Date(new Date().getTime() - 5);
         Date afterCreated = new Date(new Date().getTime() + 5);
 
-        Question q = new Question(this.author, this.ama, body);
+        Answer a = new Answer(this.amaSubject, this.ama, this.question, body);
 
-        assertEquals(q.getAuthor(), this.author);
-        assertEquals(q.getAma(), this.ama);
-        assertEquals(q.getBody(), body);
-        assertFalse(q.isEdited());
+        assertEquals(a.getAuthor(), this.amaSubject);
+        assertEquals(a.getAma(), this.ama);
+        assertEquals(a.getBody(), body);
+        assertEquals(a.getQuestion(), this.question);
+        assertFalse(a.isEdited());
 
-        assertTrue(q.getCreated() instanceof Date);
-        assertTrue(beforeCreated.before(q.getCreated()));
-        assertTrue(afterCreated.after(q.getCreated()));
+        assertTrue(a.getCreated() instanceof Date);
+        assertTrue(beforeCreated.before(a.getCreated()));
+        assertTrue(afterCreated.after(a.getCreated()));
     }
+
+
 
 }
