d51258af2f4bf8492d5193621630dcf37a0a32e0
ctblanch2 <ctblanch@gmail.com>
2017-03-16 10:25:56 -0400

    Remove unneeded code

--- src/main/java/org/sysc/ama/repo/AnswerRepository.java
+++ src/main/java/org/sysc/ama/repo/AnswerRepository.java
@@ -8,12 +8,7 @@ import org.sysc.ama.model.Answer;
 import java.util.List;
 import java.util.Optional;
 
-/**
- * Created by cameronblanchard on 3/16/2017.
- */
 public interface AnswerRepository extends CrudRepository<Answer, Long> {
 
     Optional<Answer> findById(Long id);
-
-    Optional<Answer> findByQuestion(Question question, Pageable request);
 }
