89253fbc20396505c07db6a4a27e0febc5ff6789
ctblanch2 <ctblanch@gmail.com>
2017-03-16 10:57:15 -0400

    Remove accidental check-in

--- src/test/java/org/sysc/ama/controller/AmaControllerTest.java
+++ src/test/java/org/sysc/ama/controller/AmaControllerTest.java
@@ -259,24 +259,6 @@ public class AmaControllerTest {
                 .andExpect(status().isForbidden());
     }
 
-    @Test
-    @WithUserDetails("BadUser")
-    public void testGetAnswerToQuestion () throws Exception {
-        MvcResult result = mockMvc.perform(post("/ama/" + this.amaFoo.getId() + "/question")
-                .param("body", "What is the meaning of life?")
-                .param("userId", this.testUser.getId().toString()))
-                .andReturn();
-        Integer questionId = JsonPath.parse(result.getResponse().getContentAsString()).read("$.id");
-
-        mockMvc.perform(post("/ama/" + this.amaFoo.getId() + "/question/" + questionId + "/answer")
-                .param("body", "No clue"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.body").value("No clue"));
-
-        mockMvc.perform(get("/ama/" + this.amaFoo.getId() + "/question/" + questionId + "/answers"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[?(@.body == \"No clue\")]").exists());
-    }
 
     /**
      * Sleeps the current process for the given number of milliseconds
