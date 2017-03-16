3bbedfadc3fed8625c825f47546a518816926b06
ctblanch2 <ctblanch@gmail.com>
2017-03-16 11:23:10 -0400

    Add unit test for user ID

--- src/test/java/org/sysc/ama/controller/StaticControllerTest.java
+++ src/test/java/org/sysc/ama/controller/StaticControllerTest.java
@@ -4,6 +4,7 @@ package org.sysc.ama.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import javax.annotation.PostConstruct;
 import javax.swing.text.AbstractDocument.Content;
 
 import org.junit.Test;
@@ -14,25 +15,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.sysc.ama.model.Ama;
+import org.sysc.ama.model.Question;
+import org.sysc.ama.model.User;
+import org.sysc.ama.repo.AmaRepository;
+import org.sysc.ama.repo.QuestionRepository;
+import org.sysc.ama.repo.UserRepository;
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @SpringBootTest
 public class StaticControllerTest {
-	
+
+	@Autowired
+	private UserRepository userRepo;
+
+	private User testUser;
+
+	@PostConstruct
+	public void init() {
+		this.testUser = new User("TestUser");
+		this.userRepo.save(this.testUser);
+	}
+
+
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 	@Autowired
 	private MockMvc mockMvc;
 
 	@Test
-	@WithMockUser
+	@WithUserDetails("TestUser")
 	public void test() throws Exception {
 		
 		mockMvc.perform(get("/")).andExpect(status().isOk())
 				.andExpect(view().name("index"))
-				.andExpect(model().attributeExists("userId"));
+				.andExpect(model().attribute("userId", this.testUser.getId()));
 	}
 
 }
