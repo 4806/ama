eca8431bb627efd24cdd024bf8a3e31e5f3d2608
Jeremy Dunsmore <jeremydunsmore@cmail.carleton.ca>
2017-03-22 21:43:05 -0400

    create answers

--- src/main/resources/public/scripts/question/view.js
+++ src/main/resources/public/scripts/question/view.js
@@ -7,13 +7,39 @@ window.Question = (function (Question) {
         opts = opts || {};
         this.ama = opts.ama;
         this.onDelete = opts.onDelete || function () {};
+        webix.ui( new window.Ama.Dialog({
+            id : 'win-create-answer',
+            title: 'New Answer',
+            body : new window.Ama.createForm('create-answer-form','Answer',this.createAnswer.bind(this))
+        }).view());
     }
+    
+    View.prototype.createAnswer= function (event,id){
+    	var params, el = $$('create-answer-form');
 
+        if (el.validate()) {
+            el.getTopParentView().hide();
+            params = el.getValues();
+            webix.ajax().post('/ama/' + this.ama.id + '/question/'+params.id+'/answer',params)
+
+        } else {
+            webix.message({
+                type: 'error',
+                text: 'Body cannot be empty'
+            });
+        }
+    };
+    
     View.prototype.repr = function (obj) {
         // TODO add check if user created question
-        return  'Created Date: ' + obj.created +
-            removeIcon + '<br/>' + obj.body +
-            answerButton;
+        var template=  'Created Date: ' + obj.created +
+            removeIcon + '<br/><span class="question">' + obj.body +'</span>';
+            if(!obj.answer){
+            	template +='<span class="answer"><br/><p>'+'Foo Bar'+'</p></span>';
+            }else{
+            	template += answerButton;
+            }
+            return template;
     };
 
     View.prototype.view = function () {
@@ -22,17 +48,21 @@ window.Question = (function (Question) {
             id : 'questions',
             template : this.repr.bind(this),
             type : {
-                height : 100,
+                height : 200,
                 width : 'auto'
             },
             xCount : 1,
             yCount : 10,
             onClick : {
-                'fa-trash-o' : this.onDelete.bind(this)
+                'fa-trash-o' : this.onDelete.bind(this),
+                'ans_bttn' 	 : function(event,id) {
+                	Ama.showDialog('win-create-answer');
+                	$$('create-answer-form').setValues({'id':id});
+                }
             }
         };
     };
-
+    
     Question.View = View;
     return Question;
 } (window.Question || {}));
