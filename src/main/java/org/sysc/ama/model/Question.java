28a9044db6b0f26ea78b76a95bdd8e07fd37c27f
ctblanch2 <ctblanch@gmail.com>
2017-03-22 20:31:28 -0400

    View question now includes an answer in the response

--- src/main/java/org/sysc/ama/model/Question.java
+++ src/main/java/org/sysc/ama/model/Question.java
@@ -1,13 +1,28 @@
 package org.sysc.ama.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Question extends Post {
+
+    @OneToOne(mappedBy="question")
+    @JsonManagedReference
+    private Answer answer;
 
     public Question () {}
 
     public Question (User user, Ama ama, String body) {
         super(user, ama, body);
+    }
+
+    public Answer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Answer answer) {
+        this.answer = answer;
     }
 }
--- src/test/java/org/sysc/ama/controller/AmaControllerTest.java
+++ src/test/java/org/sysc/ama/controller/AmaControllerTest.java
@@ -215,6 +215,26 @@ public class AmaControllerTest {
     }
 
     @Test
+    @WithUserDetails("TestUser")
+    public void testAnaswerIsIncludedWithQuestion () throws Exception {
+
+        MvcResult result = mockMvc.perform(post("/ama/" + this.amaFoo.getId() + "/question")
+                .param("body", "What is the meaning of life?")
+                .param("userId", this.testUser.getId().toString()))
+                .andReturn();
+        Integer questionId = JsonPath.parse(result.getResponse().getContentAsString()).read("$.id");
+
+        mockMvc.perform(post("/ama/" + this.amaFoo.getId() + "/question/" + questionId + "/answer")
+                .param("body", "No clue"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.body").value("No clue"));
+
+        mockMvc.perform(get("/ama/" + this.amaFoo.getId() + "/question/" + questionId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.answer.body").value("No clue"));
+    }
+
+    @Test
     @WithMockUser("TestUser")
     public void testDeleteQuestion () throws Exception {
 
