775592452183287bd7163f114b705f03eb4f594a
Matthew Maynes <matthew.maynes@yahoo.ca>
2017-03-30 17:39:05 -0400

    Added tests for follow/unfollow user

--- src/main/resources/public/scripts/user/view.js
+++ src/main/resources/public/scripts/user/view.js
@@ -12,11 +12,13 @@ window.User = (function (User) {
 
         if (followed) {
             template += '<span data-id="' + user.id + '" ' +
+                'data-name="' + user.name + '" ' +
                 'title="Unfollow ' + user.name + '" ' +
                 'class="webix_icon fa-close name-button"></span>';
         }
         else if (user.id !== this.user.id) {
             template += '<span data-id="' + user.id + '" ' +
+                'data-name="' + user.name +  '" ' +
                 'title="Follow ' + user.name + '" ' +
                 'class="webix_icon fa-plus name-button"></span>';
         }
@@ -26,26 +28,31 @@ window.User = (function (User) {
 
 
     View.prototype.onFollow = function (e) {
-        var user = {
-            id : e.target.getAttribute('data-id')
-        };
+        var user = getUser(e);
         webix.ajax().post('/user/follow/' + user.id)
             .then(function () {
+                webix.message('Successfully followed ' + user.name);
                 this.followed = true;
                 this.onChange();
             }.bind(this));
     };
 
     View.prototype.onUnfollow = function (e) {
-        var user = {
-            id : e.target.getAttribute('data-id')
-        };
+        var user = getUser(e);
         webix.ajax().del('/user/follow/' + user.id)
             .then(function () {
+                webix.message('Successfully unfollowed ' + user.name);
                 this.followed = false;
                 this.onChange();
             }.bind(this));
     };
+
+    function getUser (e) {
+        return {
+            id      : e.target.getAttribute('data-id'),
+            name    : e.target.getAttribute('data-name')
+        };
+    }
 
     User.View = View;
     return User;
