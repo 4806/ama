d2a50b7c3295583e207d0867d7dcb394a9bb5798
Jeremy Dunsmore <jeremydunsmore@cmail.carleton.ca>
2017-03-30 11:47:34 -0400

    closes #123

--- src/test/javascript/ama.spec.js
+++ src/test/javascript/ama.spec.js
@@ -1,7 +1,30 @@
 describe('Ama View', function () {
 
-    it('Always Passes', function () {
-        expect(true).toBeTruthy();
+    beforeEach(function () {
+        this.amas = [{
+            id: 1,
+            title: 'Ask Me',
+            subject: {
+                name: 'Foo',
+                id: 1
+            },
+            icon: '<span class="fa-trash-o webix_icon"></span>'
+        }];
+    });
+
+    it('Creates a list of amas displayed to ama subject', function () {
+        window.getUserId = function () { return 1; };
+        var listAma = new window.Ama.List({}).view();
+        listAma.data = this.amas;
+        webix.ui(listAma);
+        expect($$('ama-list').getText(1, 'icon')).toMatch('fa-trash');
+    });
+    it('Creates a list of amas displayed to non ama subject', function () {
+        window.getUserId = function () { return 2; };
+        var listAma = new window.Ama.List({}).view();
+        listAma.data = this.amas;
+        webix.ui(listAma);
+        expect($$('ama-list').getText(1, 'icon')).not.toMatch('fa-trash');
     });
 });
 
